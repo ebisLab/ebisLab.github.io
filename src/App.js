74a284b3ae5b26c817d9bd60607d2c342efcfa1c
ebisLab <ebaiden@techie.com>
2020-10-14 16:53:43 -0400

    set up animate presence

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import Home from './components/Home'
 import Init from './components/Init'
 import {BrowserRouter as Router, Route, Link} from "react-router-dom"
+import {AnimatePresence} from 'framer-motion'
 import './App.css';
 import Intro from './components/Intro';
 
@@ -10,12 +11,16 @@ function App() {
   return (
     <Router>
     <div className="App">
+    <AnimatePresence exitBeforeEnter>
       <div style={{background:"limegreen", position: "absolute",  width: "100%"}}>
         <Link to="/home">Home</Link>
       </div>
-<Intro style={{marginTop:"-50px"}}/>
+
+      <Intro style={{marginTop:"-50px"}}/>
       <Route path="/home" component={Home}/>
       <Router path="/" component={Init}/>
+      </AnimatePresence>
+
     </div>
     </Router>
   );
