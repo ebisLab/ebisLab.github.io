44c693308191ce3053f081cf7093a1eaf8ca5575
ebisLab <ebaiden@techie.com>
2020-10-14 15:45:42 -0400

    fetch request to github

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,18 @@
-import React from 'react'
+import React,{useState, useEffect} from 'react';
+import axios from 'axios'
 
 export default function Home() {
+    const [data, setData]=useState([])
+    useEffect(()=>{
+        axios.get("https://api.github.com/users/ebisLab/repos")
+        .then(res=>
+            setData(res.data))
+
+    },[])
+
+    console.log("DATA-->", data)
     return (
-        <div style={{background: "deepskyblue", margin: "0px 20px", height: "300px", position:"relative", top:"-100px"}}>
+        <div style={{background: "deepskyblue", margin: "0px 20px", position:"relative", top:"-100px"}}>
             <div style={{display:"flex"}}>
                 <div style={{margin: "20px"}}>Link 1</div>
                 <div style={{margin: "20px"}}>Link 2</div>
@@ -10,14 +20,11 @@ export default function Home() {
                 <div style={{margin: "20px"}}>Link 3</div>
             </div>
             <div style={{display:"flex", flexWrap:"wrap", flexGrow:"1"}}>
+                {data.map(item=>
+                <div className="box" key={item.id}>
+                   ( {item.name})
                 
-                <div className="box">Picture 1</div>
-                <div className="box">Picture 2</div>
-                <div className="box">Picture 3</div>
-                <div className="box">Picture 4</div>
-                <div className="box">Picture 5</div>
-                <div className="box">Picture 6</div>
-                <div className="box">Picture 7</div>
+                </div>)}
             {/* <div style={{background: "crimson" , width: "100%", padding: "20px", margin: "20px"}}>Picture 1</div>
             <div style={{background: "darksalmon" , width: "100%", padding: "20px", margin: "20px"}}>Picture 2</div>
             <div style={{background: "indianred", width: "100%", padding: "20px", margin: "20px"}}> Picture 3</div> */}
--- src/components/Intro.js
+++ src/components/Intro.js
@@ -5,8 +5,24 @@ export default function Intro() {
     return (
         <div>
             <div style={{display: "inline-flex", height:"100vh", width:"100%"}}>
-                <div style={{background: "pink", width:"100%"}}>Left part</div>
-                <div style={{background: "gold", width:"100%"}}>Right part</div>
+                <div style={{background: "pink", width:"100%"}}>
+                <div style={{position:"relative"}}>
+                        <h1 className="placement">
+                        Go to 
+                        <a href="http://www.eunicebaiden.com">E.B.'s Folio</a>
+                        </h1>
+                    </div>
+                    
+                </div>
+                <div style={{background: "gold", width:"100%"}}>
+                    <div style={{position:"relative"}}>
+                        <h1 className="placement">
+                        Continue to
+                        E.B.'s lab
+                        </h1>
+                    </div>
+                    
+                </div>
             </div>
             
         </div>
