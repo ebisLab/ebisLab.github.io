e12d671206a93665cf22e0b2cc71aec4741cee7f
ebisLab <ebaiden@techie.com>
2020-10-29 10:23:11 -0400

    Updates

---
+++ precache-manifest.1cdd58d3dae1f94953023eba00356728.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0446224c1e4eb4403ac4bb8ea6710d2e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0d446ca2e1524d0de255",
+    "url": "/static/css/main.9d58f61f.chunk.css"
+  },
+  {
+    "revision": "baec827879a86cecebdf",
+    "url": "/static/js/2.c24bd349.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "/static/js/2.c24bd349.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d446ca2e1524d0de255",
+    "url": "/static/js/main.43c74fa5.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
